e5e97018f54f5e08d701ac9bcb76785739566d08
hozsocs <hozsocs@gmail.com>
2022-08-17 17:55:58 +0200

    feladat teljesítve

--- solutions/app2.js
+++ solutions/app2.js
@@ -2,3 +2,17 @@ const user = {
   firstName: 'John',
   lastName: 'Doe',
 };
+
+const {
+  firstName: f = "unknown", 
+  lastName: l = "unknown", 
+  job: j = "unknown" 
+} = user;
+
+console.log( f, l, j);
+
+export {
+  f,
+  l,
+  j,
+};
--- solutions/app3.js
+++ solutions/app3.js
@@ -1,2 +1,9 @@
 const names = ['John', 'Jane', 'Judith', 'Jennifer', 'Jeremiah', 'Johnny'];
 
+const [,a,,b,,c]=names;
+console.log(a, b, c);
+export {
+    a,
+    b,
+    c,
+ };
